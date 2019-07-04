9fd2cca82cf8ec26259ae3ae510070edceb95a57
akshathamn <akshathamn05@gmail.com>
2019-07-04 15:15:59 +0530

    task2 completed

---
+++ Task2.js
@@ -0,0 +1,65 @@
+// Task1
+
+// const fs = require('fs')
+// fs.readFile('Task.js', 'utf-8', function (err, content) {
+//   if (err) {
+//     return console.log("something is missing")
+//   }
+
+//   console.log(content)
+// })
+
+
+
+// Task2
+
+// const fs = require('fs')
+
+// console.log('hello,good afternoon')
+
+// fs.readFile('Example.js', 'utf-8', function (err, content) {
+//   if (err) {
+//     console.log('error happened during reading the file')
+//     return console.log(err)
+//   }
+
+//   console.log(content)
+// })
+// var content2=fs.readFileSync('Example.js','utf-8',)
+// console.log(content2)
+// console.log('had your lunch?')
+
+
+
+// Task3
+
+goToHome(makePizza);
+goToHome(textMe);
+function makePizza(){
+ console.log("Pizza is ready");
+}
+function textMe(){
+  console.log("Text Message:Your Pizza Is Ready!");
+}
+function goToHome(WhatNext){
+  WhatNext();
+}
+
+
+
+
+// Task4
+// const fs = require('fs')
+
+// console.log('hello,good afternoon')
+
+// fs.readFile('Sample.txt', 'utf-8', function (err, content) {
+//   if (err) {
+//     console.log('error happened during reading the file')
+//     return console.log(err)
+//   }
+
+//   console.log(content)
+// })
+
+// console.log('had your lunch?')
